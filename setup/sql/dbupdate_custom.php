1afe6c2388d72170baafc431a81923ba40c2fc47
Jörg Lützenkirchen <luetzenkirchen@leifos.com>
2015-06-24 17:21:35 +0200

    booking pool: schedule availability

--- setup/sql/dbupdate_custom.php
+++ setup/sql/dbupdate_custom.php
@@ -614,3 +614,22 @@ if(!$ilDB->tableColumnExists('frm_posts_deleted','is_thread_deleted'))
 	);
 }
 ?>	
+<#38>
+<?php
+	if (!$ilDB->tableColumnExists("booking_schedule", "av_from"))
+	{
+		$ilDB->addTableColumn("booking_schedule", "av_from", array(
+			"type" => "integer",
+			"notnull" => false,
+			"length" => 4
+		));
+	}
+	if (!$ilDB->tableColumnExists("booking_schedule", "av_to"))
+	{
+		$ilDB->addTableColumn("booking_schedule", "av_to", array(
+			"type" => "integer",
+			"notnull" => false,
+			"length" => 4
+		));
+	}
+?>
