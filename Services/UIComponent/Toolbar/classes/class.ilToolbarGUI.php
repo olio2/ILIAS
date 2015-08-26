6b4fce0a5b84a511a1f5ce91692e2b09f0f280e6
bheyser <bheyser@databay.de>
2015-08-26 15:20:59 +0200

    fixed link button href for single link button toolbars

--- Services/UIComponent/Toolbar/classes/class.ilToolbarGUI.php
+++ Services/UIComponent/Toolbar/classes/class.ilToolbarGUI.php
@@ -660,7 +660,7 @@ class ilToolbarGUI
 				case 'button':
 					$button = ilLinkButton::getInstance();
 					$button->setCaption($item['txt'], false);
-					$button->setTarget($item['cmd']);
+					$button->setUrl($item['cmd']);
 					$button->setId($item['id']);
 					$button->setAccessKey($item['acc_key']);
 					break;
